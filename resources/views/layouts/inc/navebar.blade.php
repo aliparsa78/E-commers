17efd5347c5b519cdcccefc2b08240cb586612d5
aliparsa <aliparsa9@gmail.com>
2022-02-07 00:12:25 +0430

    Add Logout button

--- resources/views/layouts/inc/navebar.blade.php
+++ resources/views/layouts/inc/navebar.blade.php
@@ -2,7 +2,7 @@
  <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
          <a href="#">
-         <h3>E-Shop</h3>
+           <h3>E-Shop</h3>
          </a>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -12,12 +12,7 @@
             </div>
           </div>
           <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
-              </a>
-            </li>
+            
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                 <div class="sidenav-toggler-inner">
@@ -103,6 +98,28 @@
                     </div>
                   </a>
                 </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+               <i class="fa fa-user"></i>
+              </a>
+              <ul class="dropdown-menu" >
+                  <li>
+                      <a class="dropdown-item" href="#">
+                      Myprofile
+                      </a>
+                  </li>
+                  <li>
+                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                      </a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+                  </li>
+                   
+                  
               </ul>
             </li>
           </ul>
