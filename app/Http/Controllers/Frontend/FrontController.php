3a289f4852dfb743c6fa1bd1278c73afbf1a10ac
aliparsa <aliparsa9@gmail.com>
2022-05-04 16:24:42 +0430

    fasldf

--- app/Http/Controllers/Frontend/FrontController.php
+++ app/Http/Controllers/Frontend/FrontController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Catagory;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 use Auth;
 use Session;
 class FrontController extends Controller
 {
   function index(){
-      $future_product = App\Models\Product::where('trending','1')->get();
-      $trednig_catagory =App\Models\Catagory::where('popular','1')->get();
+      $future_product = Product::where('trending','1')->get();
+      $trednig_catagory =Catagory::where('popular','1')->get();
       return view('frontend.index',compact('future_product','trednig_catagory'));
   }
   function catagory(){
