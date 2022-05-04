da3029cd782b6ad61e5a3b1498b60973c25993f9
aliparsa <aliparsa9@gmail.com>
2022-05-04 16:09:36 +0430

    frontend controler

---
+++ app/Http/Controllers/Frontend/RateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RateController extends Controller
+{
+    function index(){
+        return "hello";
+    }
+}
